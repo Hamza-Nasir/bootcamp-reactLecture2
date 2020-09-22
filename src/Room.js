eadd2dc9078078919d8d59b5d60c2a708512d217
Hamza Nasir <hamzanasir123123@gmail.com>
2020-09-22 14:55:25 +0500

    Messing around with React

---
+++ src/Room.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';  
+
+function Room() {
+
+    const [isLit, setLit] = useState(true);
+    const [age, increaseAge] = useState(19);
+    // // The code snippet below does the same thing as the one commented out above
+    // const state = useState(true);
+    // //console.log("States = ", state);
+    // const isLit = state[0];
+    // const setLit = state[1];
+
+
+    // function updateLight() {
+    //     setLit(!isLit);
+    // }
+
+    // function changeAge() {
+    //     increaseAge(age+1);
+    // }
+
+  return (
+    <div>
+      Inside the Room Component. Room is {isLit? "Lit": "Dark"} <br/>
+      My Age: {age};
+
+      <br/><br/>
+      {/* <button onClick={updateLight}>Toggle the Lights</button><br/><br/>
+      <button onClick={changeAge}>Increase my Age</button> */}
+      <button onClick={() => setLit(!isLit)}>Toggle the Lights</button><br/><br/>
+      <button onClick={() => increaseAge(age+1)}>Increase my Age</button>
+    </div>
+  );
+}
+
+export default Room;
